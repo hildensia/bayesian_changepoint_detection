fcffd840ee2a13fe7aa1927b2c0b7ba4d7ef82b5
Dan Marthaler <dan.marthaler@gmail.com>
2015-06-10 13:14:21 -0700

    added Independent Factor Model

--- bayesian_changepoint_detection/offline_changepoint_detection.py
+++ bayesian_changepoint_detection/offline_changepoint_detection.py
@@ -65,7 +65,7 @@ def offline_changepoint_detection(data, prior_func,
     Q[n-1] = P[n-1, n-1]
 
     for t in reversed(range(n-1)):
-        P_next_cp = -np.inf  # == -log(0)
+        P_next_cp = -np.inf  # == log(0)
         for s in range(t, n-1):
             P[t, s] = observation_log_likelihood_function(data, t, s + 1)
 
@@ -82,7 +82,7 @@ def offline_changepoint_detection(data, prior_func,
 
         # (1 - G) is numerical stable until G becomes numerically 1
         if G[n-1-t] < -1e-15:  # exp(-1e-15) = .99999...
-            antiG = np.log(1 - np.exp(G[n-1-t])) 
+            antiG = np.log(1 - np.exp(G[n-1-t]))
         else:
             # (1 - G) is approx. -log(G) for G close to 1
             antiG = np.log(-G[n-1-t])
@@ -108,28 +108,45 @@ def gaussian_obs_log_likelihood(data, t, s):
     s += 1
     n = s - t
     mean = data[t:s].sum(0) / n
-    
+
     muT = (n * mean) / (1 + n)
     nuT = 1 + n
     alphaT = 1 + n / 2
     betaT = 1 + 0.5 * ((data[t:s] - mean) ** 2).sum(0) + ((n)/(1 + n)) * (mean**2 / 2)
     scale = (betaT*(nuT + 1))/(alphaT * nuT)
-    
+
     # splitting the PDF of the student distribution up is /much/ faster.
     # (~ factor 20) using sum over for loop is even more worthwhile
     prob = np.sum(np.log(1 + (data[t:s] - muT)**2/(nuT * scale)))
     lgA = gammaln((nuT + 1) / 2) - np.log(np.sqrt(np.pi * nuT * scale)) - gammaln(nuT/2)
-    
+
     return np.sum(n * lgA - (nuT + 1)/2 * prob)
 
+def ifm_obs_log_likelihood(data, t, s):
+    '''Independent Features model from xuan et al'''
+    s += 1
+    n = s - t
+    x = data[t:s]
+    if len(x.shape)==2:
+        d = x.shape[1]
+    else:
+        d = 1
+        x = np.atleast_2d(x).T
+
+    N0 = d          # weakest prior we can use to retain proper prior
+    V0 = np.var(x)
+    Vn = V0 + (x**2).sum(0)
+
+    # sum over dimension and return (section 3.1 from Xuan paper):
+    return d*( -(n/2)*np.log(np.pi) + (N0/2)*np.log(V0) - \
+        gammaln(N0/2) + gammaln((N0+n)/2) ) - \
+        ( ((N0+n)/2)*np.log(Vn) ).sum(0)
 
 def const_prior(r, l):
     return 1/(l)
 
-
 def geometric_prior(t, p):
     return p * ((1 - p) ** (t - 1))
-
 
 def neg_binominal_prior(t, k, p):
     return comb(t - k, k - 1) * p ** k * (1 - p) ** (t - k)
---
+++ example.py
@@ -0,0 +1,84 @@
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+
+import cProfile
+import bayesian_changepoint_detection.offline_changepoint_detection as offcd
+from functools import partial
+
+import bayesian_changepoint_detection.online_changepoint_detection as oncd
+import matplotlib.cm as cm
+
+def generate_normal_time_series(num, minl=50, maxl=1000):
+  data = np.array([], dtype=np.float64)
+  partition = np.random.randint(minl, maxl, num)
+  for p in partition:
+    mean = np.random.randn()*10
+    var = np.random.randn()*1
+    if var < 0:
+      var = var * -1
+    tdata = np.random.normal(mean, var, p)
+    data = np.concatenate((data, tdata))
+  return partition, np.atleast_2d(data).T
+
+def generate_multinormal_time_series(num, dim, minl=50, maxl=1000):
+  data = np.empty((1,dim), dtype=np.float64)
+  partition = np.random.randint(minl, maxl, num)
+  for p in partition:
+    mean = np.random.standard_normal(dim)*10
+    # Generate a random SPD matrix
+    A = np.random.standard_normal((dim,dim))
+    var = np.dot(A,A.T)
+
+    tdata = np.random.multivariate_normal(mean, var, p)
+    data = np.concatenate((data, tdata))
+  return partition, data
+
+if __name__ == '__main__':
+  show_plot = True
+  dim = 1
+  #partition, data = generate_normal_time_series(7, 50, 200)
+  partition, data = generate_multinormal_time_series(7, dim, 50, 200)
+  changes = np.cumsum(partition)
+
+  if show_plot:
+    fig, ax = plt.subplots(figsize=[16,12])
+    for p in changes:
+      ax.plot([p,p],[np.min(data),np.max(data)],'r')
+    for d in range(dim):
+      ax.plot(data[:,d])
+    plt.show()
+
+
+  #Q, P, Pcp = offcd.offline_changepoint_detection(data,partial(offcd.const_prior, l=(len(data)+1)),offcd.gaussian_obs_log_likelihood, truncate=-20)
+  Q, P, Pcp = offcd.offline_changepoint_detection(data,partial(offcd.const_prior, l=(len(data)+1)),offcd.ifm_obs_log_likelihood, truncate=-20)
+  if show_plot:
+    fig, ax = plt.subplots(figsize=[18, 16])
+    ax = fig.add_subplot(2, 1, 1)
+    for p in changes:
+      ax.plot([p,p],[np.min(data),np.max(data)],'r')
+    for d in range(dim):
+      ax.plot(data[:,d])
+    ax = fig.add_subplot(2, 1, 2, sharex=ax)
+    ax.plot(np.exp(Pcp).sum(0))
+    plt.show()
+
+  '''R, maxes = oncd.online_changepoint_detection(data,partial(oncd.constant_hazard, 250), oncd.StudentT(10, .03, 1, 0))
+
+  if show_plot:
+    fig, ax = plt.subplots(figsize=[18, 16])
+    ax = fig.add_subplot(3, 1, 1)
+    ax.plot(data)
+    for p in changes:
+      ax.plot([p,p],[np.min(data),np.max(data)],'r')
+    ax = fig.add_subplot(3, 1, 2, sharex=ax)
+    sparsity = 5  # only plot every fifth data for faster display
+    ax.pcolor(np.array(range(0, len(R[:,0]), sparsity)), \
+          np.array(range(0, len(R[:,0]), sparsity)), \
+          -np.log(R[0:-1:sparsity, 0:-1:sparsity]), \
+          cmap=cm.Greys, vmin=0, vmax=1000)
+    ax = fig.add_subplot(3, 1, 3, sharex=ax)
+    ax.plot(R[:, 1])
+    plt.show()
+  '''
